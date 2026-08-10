8331465473e65d276423490dc3372bfa20e7abd8
Kayan Shah <Kayan.shah@icloud.com>
2026-08-10 16:00:00 +0100

    Add EGBK (Sywell) taxiway routing graph

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ const GRAPH_AIRPORTS = [
   { icao: 'EGBO', lat: 52.5163, lon: -2.2617 },
   { icao: 'EGNX', lat: 52.8289, lon: -1.3326 },
   { icao: 'EGPH', lat: 55.9494, lon: -3.3615 },
+  { icao: 'EGBK', lat: 52.3055, lon: -0.7903 },
 ];
 
 function nearestAirportGraph(lat, lon) {
